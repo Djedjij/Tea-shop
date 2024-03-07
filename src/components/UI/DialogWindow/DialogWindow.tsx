75b09376608c0b5ad6d912ce4aa19d21e976eae5
Djedjij <mat3347571@gmail.com>
2024-03-07 21:53:41 +0300

    dialogWindow2

--- src/components/UI/DialogWindow/DialogWindow.tsx
+++ src/components/UI/DialogWindow/DialogWindow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./DialogWindow.module.scss";
 
 interface IChatProps {
@@ -6,6 +6,11 @@ interface IChatProps {
   setOpenChat: (openChat: boolean) => void;
 }
 
+interface IChatMessage {
+  text: string;
+  role: string;
+  time: () => string;
+}
 const DialogWindow = () => {
   const [openChat, setOpenChat] = useState(false);
   return (
@@ -26,6 +31,39 @@ const DialogWindow = () => {
 };
 
 const Chat: React.FC<IChatProps> = ({ setOpenChat }) => {
+  const getTime = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  const helloInChat: IChatMessage = {
+    text: "Здравствуйте! Чем мы можем вам помочь?",
+    role: "admin",
+    time: getTime,
+  };
+
+  const [chat, setChat] = useState<IChatMessage[]>([helloInChat]);
+  const [message, setMessage] = useState("");
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop =
+        scrollRef.current.scrollHeight - scrollRef.current.clientHeight;
+    }
+  }, [chat]);
+
+  const postMessage = (message: string) => {
+    if (message) {
+      setChat([...chat, { text: message, role: "user", time: getTime }]);
+      setMessage("");
+    }
+  };
+
   return (
     <div className={styles.chatWrapper}>
       <div className={styles.chatHeader}>
@@ -37,38 +75,41 @@ const Chat: React.FC<IChatProps> = ({ setOpenChat }) => {
           <img src="/images/icons/icon-crossWhite.svg" alt="" />
         </button>
       </div>
-      <div className={styles.chat}>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
-        <div className={styles.message}>
-          Здравствуйте! Чем мы можем вам помочь?
-        </div>
+      <div
+        ref={scrollRef}
+        className={styles.chat}
+        style={{ height: "300px", overflowY: "scroll" }}
+      >
+        {chat.map((message, index) => (
+          <div
+            className={
+              message.role === "user" ? styles.messageRight : styles.messageLeft
+            }
+            key={index}
+          >
+            <p className={styles.messageText}>{message.text}</p>
+            <p
+              className={
+                message.role === "user"
+                  ? styles.messageTimeRight
+                  : styles.messageTimeLeft
+              }
+            >
+              {message.time()}
+            </p>
+          </div>
+        ))}
       </div>
       <div className={styles.chatInput}>
-        <textarea placeholder="Введите ваше сообщение" />
-        <button className={styles.buttonInput}>
+        <textarea
+          placeholder="Введите ваше сообщение"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <button
+          className={styles.buttonInput}
+          onClick={() => postMessage(message)}
+        >
           <img src="/images/icons/icon-submitGreen.png" alt="" />
         </button>
       </div>
